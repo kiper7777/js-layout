f7d8e70b1ff75cee82b8e2d401db10772618b511
Dmytro Kiperman <8boyss@ukr.net>
2023-11-16 23:11:23 +0100

    upd

--- src/container/index/index.js
+++ src/container/index/index.js
@@ -1,96 +1,146 @@
 import {
+  createContainerTab,
   createElement,
   createHeader,
+  createMain,
 } from '../../script/layout'
-
+// ==
 const page = document.querySelector('.page')
 
 const header = createHeader()
-
 page.append(header)
 
-const title = createElement('h1', 'title', 'Мій блог')
-
+const title = createElement('h1', 'title', 'Комюніті')
 page.append(title)
 
-// ===
+const containerTab = createContainerTab()
+page.append(containerTab)
 
-const POST_LIST = [
-  {
-    category: [
-      { text: 'Важливо', id: 1 },
-      { text: 'Нова', id: 2 },
-    ],
-    info: 'До біса планувальник, наймаємо дизайнера і готуємося до презентації, як Джобс',
-    date: '25.01',
-    viewed: false,
-  },
-  {
-    category: [{ text: 'Нова', id: 2 }],
-    info: 'Ми хотіли щоб у цьому чаті було близько 150 людей щоб зробити якісний пак самопрезентацій',
-    date: '24.01',
-    viewed: true,
-  },
-]
+const containerMain = createMain()
+page.append(containerMain)
 
-const createPost = () => {
-  const postList = createElement('main', 'post__list')
+const main_info = main_info()
+page.append(main_info)
 
-  POST_LIST.forEach((postData) => {
-    const post = createElement(
-      'div',
-      postData.viewed
-        ? 'post button post--viewed'
-        : 'post button',
-    )
+// // ==================================
 
-    const postHeader = createElement('div', 'post__header')
+// import {
+//   createElement,
+//   createHeader,
+// } from '../../script/layout'
 
-    // ===
+// const page = document.querySelector('.page')
 
-    const categoryList = createElement(
-      'div',
-      'post__category-list',
-    )
+// const header = createHeader()
 
-    postData.category.forEach((category) => {
-      const categorySpan = createElement(
-        'span',
-        `post__category post__category--${category.id}`,
-        category.text,
-      )
-      categoryList.append(categorySpan)
-    })
+// page.append(header)
 
-    // ===
+// const title = createElement('h1', 'title', 'Мій блог')
 
-    const dateSpan = createElement(
-      'span',
-      'post__date',
-      postData.date,
-    )
-    postHeader.append(categoryList, dateSpan)
+// page.append(title)
 
-    // ===
+// // ===
 
-    const infoParagraph = createElement(
-      'p',
-      'post__info',
-      postData.info,
-    )
-    post.append(postHeader, infoParagraph)
+// const POST_LIST = [
+//   {
+//     category: [
+//       { text: 'Важливо', id: 1 },
+//       { text: 'Нова', id: 2 },
+//     ],
+//     info: 'До біса планувальник, наймаємо дизайнера і готуємося до презентації, як Джобс',
+//     date: '25.01',
+//     viewed: false,
+//   },
+//   {
+//     category: [{ text: 'Нова', id: 2 }],
+//     info: 'Ми хотіли щоб у цьому чаті було близько 150 людей щоб зробити якісний пак самопрезентацій',
+//     date: '24.01',
+//     viewed: true,
+//   },
+// ]
 
-    // ===
+// const createPost = () => {
+//   const postList = createElement('main', 'post__list')
 
-    postList.append(post)
-  })
+//   POST_LIST.forEach((postData) => {
+//     const post = createElement(
+//       'div',
+//       postData.viewed
+//         ? 'post button post--viewed'
+//         : 'post button',
+//     )
 
-  return postList
-}
+//     const postHeader = createElement('div', 'post__header')
 
-// ===
+//     // ===
 
-const post = createPost()
-page.append(post)
+//     const categoryList = createElement(
+//       'div',
+//       'post__category-list',
+//     )
 
-// ===
+//     postData.category.forEach((category) => {
+//       const categorySpan = createElement(
+//         'span',
+//         `post__category post__category--${category.id}`,
+//         category.text,
+//       )
+//       categoryList.append(categorySpan)
+//     })
+
+//     // ===
+
+//     const dateSpan = createElement(
+//       'span',
+//       'post__date',
+//       postData.date,
+//     )
+//     postHeader.append(categoryList, dateSpan)
+
+//     // ===
+
+//     const infoParagraph = createElement(
+//       'p',
+//       'post__info',
+//       postData.info,
+//     )
+//     post.append(postHeader, infoParagraph)
+
+//     // ===
+
+//     postList.append(post)
+//   })
+
+//   return postList
+// }
+
+// // ===
+
+// const post = createPost()
+// page.append(post)
+
+// ===================================
+
+// function openTab(evt, tabName) {
+//   // Declare all variables
+//   var i, tabcontent, tablinks
+
+//   // Get all elements with class="tabcontent" and hide them
+//   tabcontent = document.getElementsByClassName('tabcontent')
+//   for (i = 0; i < tabcontent.length; i++) {
+//     tabcontent[i].style.display = 'none'
+//   }
+
+//   // Get all elements with class="tablinks" and remove the class "active"
+//   tablinks = document.getElementsByClassName('tablinks')
+//   for (i = 0; i < tablinks.length; i++) {
+//     tablinks[i].className = tablinks[i].className.replace(
+//       'active',
+//       '',
+//     )
+//   }
+
+//   // Show the current tab, and add an "active" class to the button that opened the tab
+//   document.getElementById(tabName).style.display = 'block'
+//   evt.currentTarget.className += 'active'
+// }
--- src/script/layout.js
+++ src/script/layout.js
@@ -21,7 +21,18 @@ const HEADER_BUTTON_LIST = [
   {
     width: 24,
     height: 24,
-    src: '/img/header__avatar.png',
+    src: '/svg/header__avatar.svg',
+  },
+]
+
+const TAB_BUTTON_LIST = [
+  {
+    active: false,
+    info: 'База знань',
+  },
+  {
+    active: true,
+    info: 'Інформація',
   },
 ]
 
@@ -47,3 +58,114 @@ export const createHeader = () => {
 
   return header
 }
+// ====
+
+export const createContainerTab = () => {
+  const div = createElement('div', 'tab__list')
+
+  TAB_BUTTON_LIST.forEach((params) => {
+    const tab = createElement(
+      'div',
+      params.active
+        ? 'button tab_button tab__active'
+        : 'button tab__button',
+    )
+
+    tab.innerText = params.info
+
+    div.insertAdjacentElement('beforeend', tab)
+  })
+  return div
+}
+
+const MAIN_INFO = {
+  img: '/svg/community_img.svg',
+  title: 'Що таке база знань?',
+  text: 'База знаний — база даних, що містить правила виводу та інформацію про людський досвід і знання в певній предметній області. В самоосвітніх системах база знань також містить інформацію, яка є результатом вирішення попередніх завдань.',
+  button: `Перейти до комюніті у Телеграм`,
+}
+
+export const createMain = () => {
+  const main = createElement('main', 'main')
+
+  const img = createElement('img', 'main__img')
+  img.src = MAIN_INFO.img
+  main.appendChild(img)
+
+  const title = createElement(
+    'div',
+    'main__title',
+    MAIN_INFO.title,
+  )
+  main.appendChild(title)
+
+  const textContainer = createElement('div', 'main__text')
+
+  const paragraph = createElement(
+    'p',
+    'main__paragraph',
+    MAIN_INFO.text,
+  )
+  textContainer.appendChild(paragraph)
+  main.appendChild(textContainer)
+
+  const button = createElement(
+    'button',
+    'main__button',
+    MAIN_INFO.button,
+  )
+  main.appendChild(button)
+
+  return main
+}
+
+// // ===============================================
+// export const createElement = (tag, className, text) => {
+//   const elem = document.createElement(tag)
+
+//   if (className) {
+//     elem.className = className
+//   }
+
+//   if (text) {
+//     elem.innerHTML = text
+//   }
+
+//   return elem
+// }
+
+// const HEADER_BUTTON_LIST = [
+//   {
+//     width: 24,
+//     height: 24,
+//     src: '/svg/header__back.svg',
+//   },
+//   {
+//     width: 24,
+//     height: 24,
+//     src: '/svg/header__avatar.svg',
+//   },
+// ]
+
+// export const createHeader = () => {
+//   const header = createElement('header', 'header')
+
+//   HEADER_BUTTON_LIST.forEach((params) => {
+//     const button = createElement('button', 'button')
+
+//     const img = createElement('img')
+
+//     Object.entries(params).forEach(([key, value]) => {
+//       img[key] = value
+//     })
+
+//     // button.append(img)
+//     // header.append(button)
+
+//     button.insertAdjacentElement('beforeend', img)
+
+//     header.insertAdjacentElement('beforeend', button)
+//   })
+
+//   return header
+// }
